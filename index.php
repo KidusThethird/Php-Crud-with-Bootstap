05e749688e13698a90e70c20d5a7732ad779b2ac
kidus <kidus.z3rd@gmail.com>
2022-10-09 02:02:10 -0800

    'insert_php_added'

--- index.php
+++ index.php
@@ -20,28 +20,37 @@
       </div>
       <div class="modal-body">
         
-                    <form>
+                    <form action="insert.php" method="POST">
                     <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label  class="form-label">First Name</label>
+                    <input type="text" class="form-control" id="" name="firstName" placeholder="Enter First Name"> 
                     </div>
                     <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <label  class="form-label">Last Name</label>
+                    <input type="text" class="form-control" id="" name="lastName" placeholder="Enter Last Name"> 
                     </div>
-                    <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    <div class="mb-3">
+                    <label  class="form-label">Course</label>
+                    <input type="text" class="form-control" id="" name="course" placeholder="Enter Course"> 
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    </form>
+                    <div class="mb-3">
+                    <label  class="form-label">Phone Number</label>
+                    <input type="text" class="form-control" id="" name="contact" placeholder="Enter Phone Number"> 
+                    </div>
+                    
+                    
+                    
+                    
+                  
 
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary">Save Data</button>
       </div>
+
+      </form>
+
     </div>
   </div>
 </div>
